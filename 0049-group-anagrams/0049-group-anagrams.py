1d9c89e5dae509d0e94d38750adc485f1853b194
Gopalam-Sudheer <73033260+Gopalam-Sudheer@users.noreply.github.com>
2025-07-12 01:26:32 +0530

    Time: 14 ms (52.14%), Space: 22.7 MB (13.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,20 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         ans=[]
-        d={}
+        res=defaultdict(list)
         for i in strs:
-            l=[0 for r in range(26)]
-            for j in range(len(i)):
-                l[ord(i[j])-ord('a')]+=1
-            s=""
-            for k in range(len(l)):
-                s+=l[k]*chr(ord('a')+k)
-            if s in d:
-                d[s].append(i)
-            else:
-                d[s]=[i]
-        for i in d:
-            ans.append(d[i])
+            count=[0]*26
+            for j in i:
+                count[ord(j)-ord('a')]+=1
+            res[tuple(count)].append(i)
+        for j in res.values():
+            ans.append(j)
         return ans
         
 
