ff62b4571a839ee16b2fdea604d70ab7fe331c02
Gopalam-Sudheer <73033260+Gopalam-Sudheer@users.noreply.github.com>
2025-07-22 13:33:34 +0530

    Time: 123 ms (37.28%), Space: 18.1 MB (93.29%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,29 +1,26 @@
 class Solution:
-    def isTrue(self,d1,d2):
-        if len(d2)>len(d1):
-            return False
-        for i in d2:
-            if i not in d1 or d1[i]<d2[i]:
-                return False
-        return True
-
     def minWindow(self, s: str, t: str) -> str:
-        d1={}
-        d2={}
+        #your code goes here
+        d={}
+        c=0
+        ans=float('inf')
         for i in t:
-            d2[i]=1+d2.get(i,0)
-        start=0
-        end=0
-        ans=s+s
-        while end<len(s):
-            d1[s[end]]=1+d1.get(s[end],0)
-            while self.isTrue(d1,d2):
-                if end-start+1<len(ans):
-                    ans=s[start:end+1]
-                d1[s[start]]-=1
-                start+=1
-            end+=1
-        if ans==s+s:
-            return ""
-        else:
-            return ans
+            d[i]=d.get(i,0)+1
+        l=0
+        r=0
+        res=""
+        while r<len(s):
+            d[s[r]]=d.get(s[r],0)-1
+            if d[s[r]]>=0:
+                c+=1
+            while c==len(t):
+                d[s[l]]+=1
+                if d[s[l]]>0:
+                    c-=1
+                if r-l+1<ans:
+                    res=s[l:r+1]
+                    ans=r-l+1
+                l+=1
+            r+=1
+        return res
+                                                                                                                                                                                                                   
