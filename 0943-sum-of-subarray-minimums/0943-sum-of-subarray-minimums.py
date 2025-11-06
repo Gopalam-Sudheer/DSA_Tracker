92302fd428f43a7c2cbdf2933884dfde7aee65f7
Gopalam-Sudheer <73033260+Gopalam-Sudheer@users.noreply.github.com>
2025-11-06 21:12:26 +0530

    Time: 113 ms (26.71%), Space: 21.7 MB (52.64%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.py
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.py
@@ -1,21 +1,28 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        st=[]
-        preSmall=[-1 for i in range(len(arr))]
-        postSmall=[len(arr)]*len(arr)
+        def pse(arr,ans):
+            st=[]
+            for i in range(len(arr)):
+                while st and arr[st[-1]]>=arr[i]:
+                    st.pop()
+                if st:
+                    ans[i]=st[-1]
+                st.append(i)
+        def nse(arr,ans):
+            st=[]
+            for i in range(len(arr)-1,-1,-1):
+                while st and arr[st[-1]]>arr[i]:
+                    st.pop()
+                if st:
+                    ans[i]=st[-1]
+                st.append(i)
+        pre=[-1]*len(arr)
+        suff=[len(arr)]*len(arr)
+        pse(arr,pre)
+        nse(arr,suff)
         ans=0
+        mod=1000000007
         for i in range(len(arr)):
-            while st and arr[st[-1]]>=arr[i]:
-                st.pop()
-            if st:
-                preSmall[i]=st[-1]
-            st.append(i)
-        st=[]
-        for i in range(len(arr)-1,-1,-1):
-            while st and arr[st[-1]]>arr[i]:
-                st.pop()
-            if st:
-                postSmall[i]=st[-1]
-            st.append(i)
-            ans=(ans+(i-preSmall[i])*(postSmall[i]-i)*arr[i])%1000000007
-        return ans%1000000007
+            ans=(ans+(i-pre[i])*(suff[i]-i)*arr[i])%mod
+        return ans
+        
