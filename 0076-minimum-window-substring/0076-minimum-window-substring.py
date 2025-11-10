bc9b13f51c8d287e25fb60960cc7cd5a36c328ac
Gopalam-Sudheer <73033260+Gopalam-Sudheer@users.noreply.github.com>
2025-11-10 12:37:35 +0530

    Time: 120 ms (35.73%), Space: 18.5 MB (7.05%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,26 +1,25 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        #your code goes here
         d={}
         for i in t:
             d[i]=d.get(i,0)+1
         start=0
         end=0
-        c=0
+        n=len(s)
         ans=float('inf')
         res=""
-        while end<len(s):
+        c=0
+        while end<n:
             d[s[end]]=d.get(s[end],0)-1
             if d[s[end]]>=0:
                 c+=1
             while c>=len(t):
                 if end-start+1<ans:
-                    ans=end-start+1
                     res=s[start:end+1]
+                    ans=end-start+1
                 d[s[start]]+=1
                 if d[s[start]]>0:
                     c-=1
                 start+=1
             end+=1
         return res
-                                                                                                                                                                                                                   
