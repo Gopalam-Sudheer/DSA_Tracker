5c3c44ca13cc2f42be5d72a9dd95ed8bbe9135f3
Gopalam-Sudheer <73033260+Gopalam-Sudheer@users.noreply.github.com>
2025-11-23 01:31:43 +0530

    Time: 11 ms (57.51%), Space: 18 MB (18.77%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -12,6 +12,5 @@ class Solution:
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans=[]
-        candidates.sort()
         self.combGen(ans,[],0,target,candidates,0)
         return ans
