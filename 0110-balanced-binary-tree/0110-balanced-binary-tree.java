d8569a247141d6502d7cec603f04945af1604b40
Gopalam-Sudheer <73033260+Gopalam-Sudheer@users.noreply.github.com>
2025-12-01 07:01:31 +0530

    Time: 0 ms (100%), Space: 45.6 MB (15.18%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,18 +14,18 @@
  * }
  */
 class Solution {
-    boolean ans=true; 
     public int height(TreeNode root){
         if(root==null){
             return 0; 
         }
         int left=height(root.left);
+        if(left==-1) return -1;
         int right=height(root.right);
-        ans&=(Math.abs(left-right)<=1);
+        if(right==-1) return -1;
+        if(Math.abs(left-right)>1) return -1;
         return 1+Math.max(left,right);
     }
     public boolean isBalanced(TreeNode root) {
-        height(root);
-        return ans;
+        return height(root)!=-1;
     }
 }
