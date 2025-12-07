d2cfe7d9ea6b7d16a3933032e360b346f1997b33
Gopalam-Sudheer <73033260+Gopalam-Sudheer@users.noreply.github.com>
2025-12-07 15:57:14 +0530

    Time: 3 ms (16.78%), Space: 18 MB (70.07%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,28 +1,20 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        newColor=color
+        def isValid(r,c,mat):
+            if r<0 or c<0 or r==len(mat) or c==len(mat[0]):
+                return False
+            return True
         visited=[[False for i in range(len(image[0]))] for j in range(len(image))]
         q=deque()
-        oldColor=image[sr][sc]
         q.append((sr,sc))
-        visited[sr][sc]=True
-        image[sr][sc]=newColor
-        while len(q)!=0:
-            row,col=q.popleft()
-            if row+1<len(image) and image[row+1][col]==oldColor and visited[row+1][col]==False:
-                visited[row+1][col]=True
-                q.append((row+1,col))
-                image[row+1][col]=newColor
-            if row-1>=0 and image[row-1][col]==oldColor and visited[row-1][col]==False:
-                visited[row-1][col]=True
-                q.append((row-1,col))
-                image[row-1][col]=newColor
-            if col+1<len(image[0]) and image[row][col+1]==oldColor and visited[row][col+1]==False:
-                visited[row][col+1]=True
-                q.append((row,col+1))
-                image[row][col+1]=newColor
-            if col-1>=0 and image[row][col-1]==oldColor and visited[row][col-1]==False:
-                visited[row][col-1]=True
-                q.append((row,col-1))
-                image[row][col-1]=newColor
+        oldColor=image[sr][sc]
+        x=[-1,0,1,0]
+        y=[0,-1,0,1]
+        while q:
+            r,c=q.popleft()
+            image[r][c]=color
+            visited[r][c]=True
+            for i in range(4):
+                if isValid(r+x[i],c+y[i],image) and visited[r+x[i]][c+y[i]]==False and image[r+x[i]][c+y[i]]==oldColor:
+                    q.append((r+x[i],c+y[i]))
         return image
